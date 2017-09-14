bc91bf932b7fcf6c98f5dd75457f753fa454489f
Roman-Labtech <labtechno.roman@yandex.ru>
2017-09-14 14:35:49 +0300

    Add files via upload

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,6 @@
-// Dom7
-var $ = Dom7;
-
-// Theme
-var theme = 'auto';
-if (document.location.search.indexOf('theme=') >= 0) {
-  theme = document.location.search.split('theme=')[1].split('&')[0];
-}
-
-// Init App
-var app = new Framework7({
-  swipePanel: 'left',
+// Initialize app
+var myApp = new Framework7(
+{
   root: '#app',
   theme: theme,
   data: function () {
@@ -25,87 +16,39 @@ var app = new Framework7({
       app.dialog.alert('Hello World!');
     },
   },
-  routes: routes,  
+  routes: routes,
+}
+);
+ 
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+ 
+// Add view
+var mainView = myApp.addView('.view-main', {
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
 });
+
+
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+
+
+
+function phonecall()
+{    window.plugins.CallNumber.callNumber(onSuccess, onError,88313367613, true);
+}
 
 document.addEventListener("deviceready", onDeviceReady, false); 
 function onDeviceReady() { 
     document.addEventListener("backbutton", onBackKeyDown, false);
+    
     document.getElementById("openBrowser").addEventListener("click", openBrowser);
     document.getElementById("QRalert").addEventListener("click", dialogAlert);
     console.log(navigator.notification);
-} 
-function onBackKeyDown() { mainView.router.back(); }
-
-function ClosePanel_left(){
-$$('.panel-close').on('click', function (e) {
-        myApp.closePanel(animated);
-    });    
-  
 }
 
-function phonecall()
-{
-    window.plugins.CallNumber.callNumber(onSuccess, onError,88313367613, true);
-}
-
-function buttonweb(){
-  document.getElementById("openBrowser").addEventListener("click", openBrowser);  
-}
-
-document.getElementById("openBrowser").addEventListener("click", openBrowser);
-
-function openBrowser() {
-   var url = 'http://lab-shop.ru';
-   var target = '_blank';
-   var options = "location = yes"
-   var ref = cordova.InAppBrowser.open(url, target, options);
-   
-   ref.addEventListener('loadstart', loadstartCallback);
-   ref.addEventListener('loadstop', loadstopCallback);
-   ref.addEventListener('loadloaderror', loaderrorCallback);
-   ref.addEventListener('exit', exitCallback);
-
-   function loadstartCallback(event) {
-      console.log('Loading started: '  + event.url)
-   }
-
-   function loadstopCallback(event) {
-      console.log('Loading finished: ' + event.url)
-   }
-
-   function loaderrorCallback(error) {
-      console.log('Loading error: ' + error.message)
-   }
-
-   function exitCallback() {
-      console.log('Browser is closed...')
-   }
-}
-
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    )};
