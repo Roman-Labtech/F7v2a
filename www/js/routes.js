21b97a1fa5989e3b86a1749c82751d52f4736ab0
Roman-Labtech <labtechno.roman@yandex.ru>
2017-09-14 09:49:04 +0300

    Add files via upload

---
+++ www/js/routes.js
@@ -0,0 +1,637 @@
+var routes = [
+  // Index page
+  {
+    path: '/',
+    url: './index.html',
+  },
+  // About page
+  {
+    path: '/about/',
+    url: './pages/about.html',
+  },
+  // Right Panel pages
+  {
+    path: '/panel-right-1/',
+    content: '\
+      <div class="page">\
+        <div class="navbar">\
+          <div class="navbar-inner sliding">\
+            <div class="left">\
+              <a href="#" class="link back">\
+                <i class="icon icon-back"></i>\
+                <span class="ios-only">Back</span>\
+              </a>\
+            </div>\
+            <div class="title">Panel Page 1</div>\
+          </div>\
+        </div>\
+        <div class="page-content">\
+          <div class="block">\
+            <p>This is a right panel page 1</p>\
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
+          </div>\
+        </div>\
+      </div>\
+    ',
+  },
+  {
+    path: '/panel-right-2/',
+    content: '\
+      <div class="page">\
+        <div class="navbar">\
+          <div class="navbar-inner sliding">\
+            <div class="left">\
+              <a href="#" class="link back">\
+                <i class="icon icon-back"></i>\
+                <span class="ios-only">Back</span>\
+              </a>\
+            </div>\
+            <div class="title">Panel Page 2</div>\
+          </div>\
+        </div>\
+        <div class="page-content">\
+          <div class="block">\
+            <p>This is a right panel page 2</p>\
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
+          </div>\
+        </div>\
+      </div>\
+    ',
+  },
+
+  // Components
+               {
+    path: '/microlist/',
+    url: './pages/microlist.html',
+  }, 
+         {
+    path: '/m50luer/',
+    url: './pages/micro/m50luer.html',
+  },
+    {
+    path: '/m100luer/',
+    url: './pages/micro/m100luer.html',
+  }, 
+         {
+    path: '/m250luer/',
+    url: './pages/micro/m250luer.html',
+  },
+            {
+    path: '/m500luer/',
+    url: './pages/micro/m500luer.html',
+  },
+            {
+    path: '/m1000luer/',
+    url: './pages/micro/m1000luer.html',
+  },
+            {
+    path: '/m2000luer/',
+    url: './pages/micro/m2000luer.html',
+  },
+            {
+    path: '/m5000luer/',
+    url: './pages/micro/m5000luer.html',
+  },
+            {
+    path: '/m1000luer/',
+    url: './pages/micro/m10000luer.html',
+  },
+            {
+    path: '/m20000luer/',
+    url: './pages/micro/m20000luer.html',
+  },
+    {
+    path: '/viscolist1/',
+    url: './pages/viscolist1.html',
+  },    
+     {
+    path: '/m100/',
+    url: './pages/micro/m100.html',
+  },
+  {
+    path: '/m50n/',
+    url: './pages/micro/m50n.html',
+  },
+    {
+    path: '/m50/',
+    url: './pages/micro/m50.html',
+  }, 
+ {
+    path: '/m10n/',
+    url: './pages/micro/m10n.html',
+  },    
+  {
+    path: '/m10/',
+    url: './pages/micro/m10.html',
+  },
+      {
+    path: '/m5n/',
+    url: './pages/micro/m5n.html',
+  },
+      {
+    path: '/m5/',
+    url: './pages/micro/m5.html',
+  },
+      {
+    path: '/m1n/',
+    url: './pages/micro/m1n.html',
+  },
+    {
+    path: '/m1/',
+    url: './pages/micro/m1.html',
+  },     
+    
+           {
+    path: '/ppm/',
+    url: './pages/eda/ppm.html',
+  },   
+               {
+    path: '/pprmt/',
+    url: './pages/eda/pprmt.html',
+  }, 
+               {
+    path: '/pprmz/',
+    url: './pages/eda/pprmz.html',
+  }, 
+               {
+    path: '/ppsm1/',
+    url: './pages/eda/ppsm1.html',
+  }, 
+      {
+    path: '/ppsm2/',
+    url: './pages/eda/ppsm2.html',
+  }, 
+    
+    {
+    path: '/pdk1/',
+    url: './pages/kraski/pdk1.html',
+  }, 
+        {
+    path: '/pdk2/',
+    url: './pages/kraski/pdk2.html',
+  }, 
+    {
+    path: '/contacts/',
+    url: './pages/contacts.html',
+  }, 
+       {
+    path: '/gaslist/',
+    url: './pages/gaslist.html',
+  },
+           {
+    path: '/neftlist/',
+    url: './pages/neftlist.html',
+  },
+           {
+    path: '/edalist/',
+    url: './pages/edalist.html',
+  },
+    
+       {
+    path: '/agresslist/',
+    url: './pages/agresslist.html',
+  },
+            {
+    path: '/paintlist/',
+    url: './pages/paintlist.html',
+  }, 
+        {
+    path: '/waterlist/',
+    url: './pages/waterlist.html',
+  }, 
+    {
+    path: '/problist/',
+    url: './pages/problist.html',
+  },    
+    
+    {
+    path: '/viscolist/',
+    url: './pages/viscolist.html',
+  },
+        {
+    path: '/sec/',
+    url: './pages/visco/sec.html',
+  },     
+    {
+    path: '/vms/',
+    url: './pages/visco/vms.html',
+  }, 
+        {
+    path: '/vz4s/',
+    url: './pages/visco/vz4s.html',
+  }, 
+        {
+    path: '/vz4p/',
+    url: './pages/visco/vz4p.html',
+  }, 
+        {
+    path: '/vz4t/',
+    url: './pages/visco/vz4t.html',
+  }, 
+        {
+    path: '/vz246pf/',
+    url: './pages/visco/vz246pf.html',
+  }, 
+        {
+    path: '/vz246tf/',
+    url: './pages/visco/vz246tf.html',
+  }, 
+        {
+    path: '/vz246sf/',
+    url: './pages/visco/vz246sf.html',
+  }, 
+        {
+    path: '/vz246p/',
+    url: './pages/visco/vz246p.html',
+  }, 
+    {
+    path: '/vz246t/',
+    url: './pages/visco/vz246t.html',
+  },   
+    {
+    path: '/vz246s/',
+    url: './pages/visco/vz246s.html',
+  },
+    {
+    path: '/accordion/',
+    url: './pages/accordion.html',
+  },
+  {
+    path: '/action-sheet/',
+    componentUrl: './pages/action-sheet.html',
+  },
+  {
+    path: '/autocomplete/',
+    componentUrl: './pages/autocomplete.html',
+  },
+  {
+    path: '/badge/',
+    componentUrl: './pages/badge.html',
+  },
+  {
+    path: '/buttons/',
+    url: './pages/buttons.html',
+  },
+  {
+    path: '/cards/',
+    url: './pages/cards.html',
+  },
+  {
+    path: '/checkbox/',
+    url: './pages/checkbox.html',
+  },
+  {
+    path: '/chips/',
+    componentUrl: './pages/chips.html',
+  },
+  {
+    path: '/contacts-list/',
+    url: './pages/contacts-list.html',
+  },
+  {
+    path: '/content-block/',
+    url: './pages/content-block.html',
+  },
+  {
+    path: '/data-table/',
+    componentUrl: './pages/data-table.html',
+  },
+  {
+    path: '/dialog/',
+    componentUrl: './pages/dialog.html',
+  },
+  {
+    path: '/fab/',
+    url: './pages/fab.html',
+  },
+  {
+    path: '/fab-morph/',
+    url: './pages/fab-morph.html',
+  },
+  {
+    path: '/form-storage/',
+    url: './pages/form-storage.html',
+  },
+  {
+    path: '/grid/',
+    url: './pages/grid.html',
+  },
+  {
+    path: '/icons/',
+    componentUrl: './pages/icons.html',
+  },
+  {
+    path: '/infinite-scroll/',
+    componentUrl: './pages/infinite-scroll.html',
+  },
+  {
+    path: '/inputs/',
+    url: './pages/inputs.html',
+  },
+  {
+    path: '/lazy-load/',
+    url: './pages/lazy-load.html',
+  },
+  {
+    path: '/list/',
+    url: './pages/list.html',
+  },
+  {
+    path: '/login-screen/',
+    componentUrl: './pages/login-screen.html',
+  },
+  {
+    path: '/login-screen-page/',
+    componentUrl: './pages/login-screen-page.html',
+  },
+  {
+    path: '/messages/',
+    componentUrl: './pages/messages.html',
+  },
+  {
+    path: '/navbar/',
+    url: './pages/navbar.html',
+  },
+  {
+    path: '/navbar-hide-scroll/',
+    url: './pages/navbar-hide-scroll.html',
+  },
+  {
+    path: '/notifications/',
+    componentUrl: './pages/notifications.html',
+  },
+  {
+    path: '/panel/',
+    url: './pages/panel.html',
+  },
+  {
+    path: '/photo-browser/',
+    componentUrl: './pages/photo-browser.html',
+  },
+  {
+    path: '/popup/',
+    componentUrl: './pages/popup.html',
+  },
+  {
+    path: '/popover/',
+    url: './pages/popover.html',
+  },
+  {
+    path: '/preloader/',
+    componentUrl: './pages/preloader.html',
+  },
+  {
+    path: '/progressbar/',
+    componentUrl: './pages/progressbar.html',
+  },
+  {
+    path: '/pull-to-refresh/',
+    componentUrl: './pages/pull-to-refresh.html',
+  },
+  {
+    path: '/radio/',
+    url: './pages/radio.html',
+  },
+  {
+    path: '/range/',
+    componentUrl: './pages/range.html',
+  },
+  {
+    path: '/searchbar/',
+    url: './pages/searchbar.html',
+  },
+  {
+    path: '/searchbar-expandable/',
+    url: './pages/searchbar-expandable.html',
+  },
+  {
+    path: '/sheet-modal/',
+    componentUrl: './pages/sheet-modal.html',
+  },
+  {
+    path: '/smart-select/',
+    url: './pages/smart-select.html',
+  },
+  {
+    path: '/sortable/',
+    url: './pages/sortable.html',
+  },
+  {
+    path: '/statusbar/',
+    componentUrl: './pages/statusbar.html',
+  },
+  {
+    path: '/subnavbar/',
+    url: './pages/subnavbar.html',
+  },
+  {
+    path: '/subnavbar-title/',
+    url: './pages/subnavbar-title.html',
+  },
+  {
+    path: '/swiper/',
+    url: './pages/swiper.html',
+    routes: [
+      {
+        path: 'swiper-horizontal/',
+        url: './pages/swiper-horizontal.html',
+      },
+      {
+        path: 'swiper-vertical/',
+        url: './pages/swiper-vertical.html',
+      },
+      {
+        path: 'swiper-space-between/',
+        url: './pages/swiper-space-between.html',
+      },
+      {
+        path: 'swiper-multiple/',
+        url: './pages/swiper-multiple.html',
+      },
+      {
+        path: 'swiper-nested/',
+        url: './pages/swiper-nested.html',
+      },
+      {
+        path: 'swiper-loop/',
+        url: './pages/swiper-loop.html',
+      },
+      {
+        path: 'swiper-3d-cube/',
+        url: './pages/swiper-3d-cube.html',
+      },
+      {
+        path: 'swiper-3d-coverflow/',
+        url: './pages/swiper-3d-coverflow.html',
+      },
+      {
+        path: 'swiper-3d-flip/',
+        url: './pages/swiper-3d-flip.html',
+      },
+      {
+        path: 'swiper-fade/',
+        url: './pages/swiper-fade.html',
+      },
+      {
+        path: 'swiper-scrollbar/',
+        url: './pages/swiper-scrollbar.html',
+      },
+      {
+        path: 'swiper-gallery/',
+        componentUrl: './pages/swiper-gallery.html',
+      },
+      {
+        path: 'swiper-custom-controls/',
+        url: './pages/swiper-custom-controls.html',
+      },
+      {
+        path: 'swiper-parallax/',
+        url: './pages/swiper-parallax.html',
+      },
+      {
+        path: 'swiper-lazy/',
+        url: './pages/swiper-lazy.html',
+      },
+      {
+        path: 'swiper-pagination-progress/',
+        url: './pages/swiper-pagination-progress.html',
+      },
+      {
+        path: 'swiper-pagination-fraction/',
+        url: './pages/swiper-pagination-fraction.html',
+      },
+      {
+        path: 'swiper-zoom/',
+        url: './pages/swiper-zoom.html',
+      },
+    ],
+  },
+  {
+    path: '/swipeout/',
+    componentUrl: './pages/swipeout.html',
+  },
+  {
+    path: '/tabs/',
+    url: './pages/tabs.html',
+  },
+  {
+    path: '/tabs-static/',
+    url: './pages/tabs-static.html',
+  },
+  {
+    path: '/tabs-animated/',
+    url: './pages/tabs-animated.html',
+  },
+  {
+    path: '/tabs-swipeable/',
+    url: './pages/tabs-swipeable.html',
+  },
+  {
+    path: '/tabs-routable/',
+    url: './pages/tabs-routable.html',
+    tabs: [
+      {
+        path: '/',
+        id: 'tab1',
+        content: ' \
+        <div class="block"> \
+          <p>Tab 1 content</p> \
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam enim quia molestiae facilis laudantium voluptates obcaecati officia cum, sit libero commodi. Ratione illo suscipit temporibus sequi iure ad laboriosam accusamus?</p> \
+          <p>Saepe explicabo voluptas ducimus provident, doloremque quo totam molestias! Suscipit blanditiis eaque exercitationem praesentium reprehenderit, fuga accusamus possimus sed, sint facilis ratione quod, qui dignissimos voluptas! Aliquam rerum consequuntur deleniti.</p> \
+          <p>Totam reprehenderit amet commodi ipsum nam provident doloremque possimus odio itaque, est animi culpa modi consequatur reiciendis corporis libero laudantium sed eveniet unde delectus a maiores nihil dolores? Natus, perferendis.</p> \
+        </div> \
+        ',
+      },
+      {
+        path: '/tab2/',
+        id: 'tab2',
+        content: '\
+        <div class="block"> \
+          <p>Tab 2 content</p> \
+          <p>Suscipit, facere quasi atque totam. Repudiandae facilis at optio atque, rem nam, natus ratione cum enim voluptatem suscipit veniam! Repellat, est debitis. Modi nam mollitia explicabo, unde aliquid impedit! Adipisci!</p> \
+          <p>Deserunt adipisci tempora asperiores, quo, nisi ex delectus vitae consectetur iste fugiat iusto dolorem autem. Itaque, ipsa voluptas, a assumenda rem, dolorum porro accusantium, officiis veniam nostrum cum cumque impedit.</p> \
+          <p>Laborum illum ipsa voluptatibus possimus nesciunt ex consequatur rem, natus ad praesentium rerum libero consectetur temporibus cupiditate atque aspernatur, eaque provident eligendi quaerat ea soluta doloremque. Iure fugit, minima facere.</p> \
+        </div> \
+        ',
+      },
+      {
+        path: '/tab3/',
+        id: 'tab3',
+        content: '\
+        <div class="block"> \
+          <p>Tab 3 content</p> \
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam enim quia molestiae facilis laudantium voluptates obcaecati officia cum, sit libero commodi. Ratione illo suscipit temporibus sequi iure ad laboriosam accusamus?</p> \
+          <p>Deserunt adipisci tempora asperiores, quo, nisi ex delectus vitae consectetur iste fugiat iusto dolorem autem. Itaque, ipsa voluptas, a assumenda rem, dolorum porro accusantium, officiis veniam nostrum cum cumque impedit.</p> \
+          <p>Laborum illum ipsa voluptatibus possimus nesciunt ex consequatur rem, natus ad praesentium rerum libero consectetur temporibus cupiditate atque aspernatur, eaque provident eligendi quaerat ea soluta doloremque. Iure fugit, minima facere.</p> \
+        </div> \
+        ',
+      },
+    ],
+  },
+  {
+    path: '/toast/',
+    componentUrl: './pages/toast.html',
+  },
+  {
+    path: '/toggle/',
+    url: './pages/toggle.html',
+  },
+  {
+    path: '/toolbar-tabbar/',
+    componentUrl: './pages/toolbar-tabbar.html',
+    routes: [
+      {
+        path: 'tabbar/',
+        componentUrl: './pages/tabbar.html',
+      },
+      {
+        path: 'tabbar-labels/',
+        componentUrl: './pages/tabbar-labels.html',
+      },
+      {
+        path: 'tabbar-scrollable/',
+        componentUrl: './pages/tabbar-scrollable.html',
+      },
+      {
+        path: 'toolbar-hide-scroll/',
+        url: './pages/toolbar-hide-scroll.html',
+      },
+    ],
+  },
+  {
+    path: '/timeline/',
+    url: './pages/timeline.html',
+  },
+  {
+    path: '/timeline-vertical/',
+    url: './pages/timeline-vertical.html',
+  },
+  {
+    path: '/timeline-horizontal/',
+    url: './pages/timeline-horizontal.html',
+  },
+  {
+    path: '/timeline-horizontal-calendar/',
+    url: './pages/timeline-horizontal-calendar.html',
+  },
+  {
+    path: '/virtual-list/',
+    componentUrl: './pages/virtual-list.html',
+  },
+
+  // Color Themes
+  {
+    path: '/color-themes/',
+    componentUrl: './pages/color-themes.html',
+  },
+
+  // Page Loaders
+  {
+    path: '/page-loader-template7/:user/:userId/:posts/:postId/',
+    templateUrl: './pages/page-loader-template7.html',
+  },
+  {
+    path: '/page-loader-component/:user/:userId/:posts/:postId/',
+    componentUrl: './pages/page-loader-component.html',
+  },
+];
